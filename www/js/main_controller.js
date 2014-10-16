acc76d94a82a9f49eca9c3b891bdc08abbdd8245
Mikey Badr <mfbadr@gmail.com>
2014-10-16 10:01:01 -0500

    init

---
+++ www/js/main_controller.js
@@ -0,0 +1,9 @@
+(function(){
+  'use strict';
+
+  angular.module('pot-o-gold')
+  .controller('MainCtrl', ['$scope', '$interval', function($scope, $interval){
+    $scope.title = 'Pot-O-Gold';
+
+  }]);
+})();
