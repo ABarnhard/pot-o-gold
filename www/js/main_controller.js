720fc02a2bb257703a325e3608e4a0d923ae72a6
Adam Barnhard <admbarn@gmail.com>
2014-10-16 12:05:51 -0500

    added partials

--- www/js/main_controller.js
+++ www/js/main_controller.js
@@ -4,6 +4,7 @@
   angular.module('pot-o-gold')
   .controller('MainCtrl', ['$scope', '$interval', function($scope, $interval){
     $scope.title = 'Pot-O-Gold';
-    var id = navigator.gyroscope.watchAngularSpeed(function(){}, function(){}, {frequency:500})
+    // var id = navigator.gyroscope.watchAngularSpeed(function(){}, function(){}, {frequency:500});
+    console.log(navigator);
   }]);
 })();
