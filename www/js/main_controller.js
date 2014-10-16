54c585486c99a2ab7aecca48052ea49c91ed9d06
Adam Barnhard <admbarn@gmail.com>
2014-10-16 11:30:35 -0500

    test file change

--- www/js/main_controller.js
+++ www/js/main_controller.js
@@ -4,6 +4,6 @@
   angular.module('pot-o-gold')
   .controller('MainCtrl', ['$scope', '$interval', function($scope, $interval){
     $scope.title = 'Pot-O-Gold';
-
+    var id = navigator.gyroscope.watchAngularSpeed(function(){}, function(){}, {frequency:500})
   }]);
 })();
